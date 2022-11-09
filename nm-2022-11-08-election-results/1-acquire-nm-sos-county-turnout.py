2b4424b8ef0fd383749849aa89c0b5957c54d6a0
Chris Keller <christopherlawrencekeller@gmail.com>
2022-11-08 23:19:11 -0700

    updates data pipeline to get data from nm sos

--- nm-2022-11-08-election-results/1-acquire-nm-sos-county-turnout.py
+++ nm-2022-11-08-election-results/1-acquire-nm-sos-county-turnout.py
@@ -15,22 +15,27 @@ logging.basicConfig(
 
 """
 This script pulls down a json from the New Mexico
-Secretary of State election results data for the
-2022-11-08 general election
+Secretary of State with county by county voter turnout
+data from the 2022-11-08 general election
 """
 
 timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H%M%S")
 
 dir_current = os.path.dirname(os.path.realpath(__file__))
 
-dir_data = "results-csv"
+dir_json = "turnout-json"
 
-file_output = '{0}-nm-sos-results.csv'.format(timestamp)
+file_output = '{0}-nm-sos-turnout.json'.format(timestamp)
 
-file_saved = os.path.join(dir_current, dir_data, file_output)
+file_saved = os.path.join(dir_current, dir_json, file_output)
 
-target_url = "https://electionresults.sos.state.nm.us/resultsCSV.aspx?text=All&type=STATE&map=CTY"
+target_url = "https://nmresws.azurewebsites.net/NMResultsAjax.svc/GetVoterTurnoutData?nocache={0}".format(timestamp.replace("-", ""))
 
-response = urllib.request.urlretrieve(target_url, file_saved)
+response = urllib.request.urlopen(target_url)
+
+raw = json.load(response)
+
+with open(file_saved, 'w', encoding='utf-8') as f:
+    json.dump(raw, f, ensure_ascii=False, indent=4)
 
 logger.debug('File saved to {0}'.format(file_saved))
--- nm-2022-11-08-election-results/2-compile-nm-sos-county-turnout.py
+++ nm-2022-11-08-election-results/2-compile-nm-sos-county-turnout.py
@@ -21,13 +21,13 @@ class BuildLatestCsv(object):
 
     """
     This script creates a csv from the latest json file
-    of election results from the New Mexico Secretary of State
-    for the 2022-11-08 general election
+    of county by county turnout from the New Mexico
+    Secretary of State for the 2022-11-08 general election
     """
 
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H%M%S")
     dir_current = os.path.dirname(os.path.realpath(__file__))
-    dir_data = "results-json"
+    dir_data = "turnout-json"
     path = os.path.join(dir_current, dir_data)
 
     def handle(self):
@@ -39,11 +39,9 @@ class BuildLatestCsv(object):
         with open(target, encoding='utf-8') as f:
             raw_json = json.load(f)
             for item in raw_json:
-                item['name'] = item['name'].title()
-                item['label'] = item['label'].title()
                 item['acquired_datestamp'] = self.timestamp
                 latest_output.append(item)
-        latest_csv = "latest-nm-sos-results.csv"
+        latest_csv = "latest-nm-sos-turnout.csv"
 
         # write the data to a file
         self.write_csv(latest_csv, latest_output)
