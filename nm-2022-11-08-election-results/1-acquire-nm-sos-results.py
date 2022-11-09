70ef7b9878958947a2efd308cce847f0e2ac253e
Chris Keller <christopherlawrencekeller@gmail.com>
2022-11-08 19:37:07 -0700

    adds script and github action to pull down new mexico election results from 2022-11-08 from the secretary of state website.

---
+++ nm-2022-11-08-election-results/1-acquire-nm-sos-results.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import urllib.request
+import json
+import datetime
+import os
+import logging
+
+logger = logging.getLogger("root")
+logging.basicConfig(
+    format="\033[1;36m%(levelname)s: %(filename)s (def %(funcName)s %(lineno)s): \033[1;37m %(message)s",
+    level=logging.DEBUG
+)
+
+"""
+This script pulls down a json from the New Mexico
+Secretary of State election results data for the
+2022-11-08 general election
+"""
+
+timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H%M%S")
+
+dir_current = os.path.dirname(os.path.realpath(__file__))
+
+dir_json = "results-json"
+
+file_output = '{0}-nm-sos-results.json'.format(timestamp)
+
+file_saved = os.path.join(dir_current, dir_json, file_output)
+
+target_url = "https://nmresws.azurewebsites.net/NMResultsAjax.svc/GetCandidates?ElectionType=General&ElectionID=0&nocache={0}".format(timestamp.replace("-", ""))
+
+logger.debug(target_url)
+
+response = urllib.request.urlopen(target_url)
+
+raw = json.load(response)
+
+with open(file_saved, 'w', encoding='utf-8') as f:
+    json.dump(raw, f, ensure_ascii=False, indent=4)
+
+logger.debug('File saved to {0}'.format(file_saved))
